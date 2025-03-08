b16a3676536cdfb828a6880ee347a8e4bc47d2a7
devnight <devnight.learn@gmail.com>
2025-03-08 05:03:45 +0330

    create page login & page register

--- components/header/Login.jsx
+++ components/header/Login.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import iconSearch from "../../public/img/search-normal.svg"
+import Link from "next/link";
 
 export function Login() {
   return (
@@ -7,9 +8,9 @@ export function Login() {
       <button id="searchIcon" className="bg-gray-100 rounded-full p-2 hover:bg-blue-50 transition-all">
         <Image src={iconSearch} width={24} height={24} alt="search icon" />
       </button>
-      <button className="p-2 bg-blue-600 text-white rounded-md hover:bg-blue-900 transition-all">
+      <Link href="/login" className="p-2 bg-blue-600 text-white rounded-md hover:bg-blue-900 transition-all">
         ثبت نام و ورود
-      </button>
+      </Link>
 
 
 
