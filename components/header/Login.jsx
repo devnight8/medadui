ee91bb42eade0c9f09a4923fa4f197f4ec675ff9
devnight <devnight.learn@gmail.com>
2025-03-10 05:27:18 +0330

    modal search is added

--- components/header/Login.jsx
+++ components/header/Login.jsx
@@ -4,24 +4,24 @@ import Image from "next/image";
 import iconSearch from "../../public/img/search-normal.svg"
 import Link from "next/link";
 import { useState } from "react";
+import ModalSearchBar from "./ModalSearchBar";
 
 export function Login() {
   const [showAlert, setShowAlert] = useState(false)
   function showModalSearch() {
-    setShowAlert(true)
-    setTimeout(() => {
-      setShowAlert(false);
-    }, 2000);
+    setShowAlert(prevShowAlert => !prevShowAlert);
+
   }
   return (
     <div className="flex items-center justify-center gap-4">
-      {showAlert ? (<p className="absolute top-24 z-10 rounded text-center left-0 right-0 p-2 bg-red-400 text-white m-auto w-fit px-20 ">سرچ بار فعال نیست !</p>) : null}
+      {showAlert ? <ModalSearchBar /> : null}
       <button onClick={showModalSearch} className="bg-gray-100 rounded-full p-2 hover:bg-blue-50 transition-all" >
         <Image src={iconSearch} width={24} height={24} alt="search icon" />
       </button>
       <Link href="/login" className="p-2 bg-blue-600 text-white rounded-md hover:bg-blue-900 transition-all">
         ثبت نام و ورود
       </Link>
+
     </div>
 
   );
